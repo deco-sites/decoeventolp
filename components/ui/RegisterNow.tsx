0a96acaa61b31c35ddccbb70620e0f167cc96cec
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2024-05-21 19:18:17 -0300

    Fix: Ajusta o register now

--- components/ui/RegisterNow.tsx
+++ components/ui/RegisterNow.tsx
@@ -19,16 +19,14 @@ export default function RegisterNow(props: Props) {
       <div class="xl:container xl:mx-auto mx-5 md:mx-10 flex flex-col gap-4 lg:gap-8">
         <div class="w-full text-center items-center">
           <div class="overflow-hidden pt-2">
-            {list.map((text: string, index: number) => (
-              <div
-                class={`flex flex-row flex-nowrap w-full animate-walk ${
-                  index > 0 && index < list.length - 1 ? "py-6" : "py-2"
-                } items-center gap-4`}
-                style={`animation-delay: ${300 * index}ms`}
-              >
+            <div
+              class={`flex flex-row flex-nowrap w-full animate-walk items-center gap-4`}
+              style={`animation-delay: 300ms`}
+            >
+              {list.map((text: string, index: number) => (
                 <h4>{text}</h4>
+              ))}
               </div>
-            ))}
           </div>
         </div>
       </div>
