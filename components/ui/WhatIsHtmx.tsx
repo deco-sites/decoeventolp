a118be9e166fdcd74e62859f14ebf641017ec3e0
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2024-05-24 18:07:42 -0300

    Fix: Ajusta o referral, hero e are you ready, ajusta tbm o número de cards no categoru

--- components/ui/WhatIsHtmx.tsx
+++ components/ui/WhatIsHtmx.tsx
@@ -2,6 +2,10 @@ import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import HTMLRenderer, { HTML } from "deco-sites/std/components/HTMLRenderer.tsx";
 
+export interface SectionProps {
+  cards?: WhatIsHtmxProps[]
+}
+
 export interface WhatIsHtmxProps {
   titleImage?: ImageWidget;
   titleText?: HTML;
@@ -29,57 +33,59 @@ const BTN_COLORS = {
 }
 
 export default function WhatIsHtmx(
-  { titleText = '', titleImage = '', description = '', cta, cards, bottomMessage }: WhatIsHtmxProps,
+  { cards }: SectionProps,
 ) {
   return (
     <div class="w-full bg-black py-5 md:py-28">
-      <div class="xl:container xl:mx-auto mx-5 md:mx-10 flex flex-col items-center justify-center gap-6 md:gap-8">
-        { titleImage ? (
-          <div className="flex justify-center">
-            <Image src={titleImage ?? ""} alt="HTMX" width={224} height={44} />
-          </div>
-        ) : null }
-        <div class="flex flex-col items-center gap-3">
-          <p class="text-[#FFF] font-semibold text-[32px] md:text-[40px]">
-            <HTMLRenderer html={titleText ?? ""} />
-          </p>
-          <HTMLRenderer html={description ?? ""} />
-        </div>
-        <div className="flex flex-col md:flex-row gap-4 mb-6">
-          {cta?.map(({ color = 'neutral', href = '', label = '' }) => (
-            <a
-              className={`flex items-center font-medium text-[16px] px-8 py-3 rounded-full md:transition md:ease-in-out md:duration-300 font-semibold
-              ${BTN_COLORS[color]}
-              `}
-              href={href}
-            >
-              {label}
-            </a>
-          ))}
-        </div>
-        <div class="grid flex-row wrap justify-center gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {cards?.map((
-            { icon, title, description }: CardProps,
-            index: number,
-          ) => (
-            <div
-              key={index}
-              class="bg-[#000D0D] flex flex-col justify-center items-start gap-2 md:gap-6 p-6 md:p-10 py-12 text-[#FFFFFF] rounded-2xl"
-            >
-              {icon && <div className="rounded-full p-1 bg-[#113032]"><img src={icon} width={32} height={32} /></div>}
-              {title && (
-                <p class="font-semibold mt-2 md:mt-0 text-[24px]">{title}</p>
-              )}
-              {description && <p class="text-[20px] text-[#A1A1AA]">{description}</p>}
+      { cards?.map(({ titleImage = '', titleText = '', description = '', cta, bottomMessage = '' }) => (
+        <div class="xl:container xl:mx-auto mx-5 md:mx-10 flex flex-col items-center justify-center gap-6 md:gap-8">
+          { titleImage ? (
+            <div className="flex justify-center">
+              <Image src={titleImage ?? ""} alt="HTMX" width={224} height={44} />
             </div>
-          ))}
-        </div>
-        { bottomMessage ? (
-          <div className="text-[30px] text-center">
-            <HTMLRenderer html={bottomMessage} />
+          ) : null }
+          <div class="flex flex-col items-center gap-3">
+            <p class="text-[#FFF] font-semibold text-[32px] md:text-[40px]">
+              <HTMLRenderer html={titleText ?? ""} />
+            </p>
+            <HTMLRenderer html={description ?? ""} />
           </div>
-        ) : null }
-      </div>
+          <div className="flex flex-col md:flex-row gap-4 mb-6">
+            {cta?.map(({ color = 'neutral', href = '', label = '' }) => (
+              <a
+                className={`flex items-center font-medium text-[16px] px-8 py-3 rounded-full md:transition md:ease-in-out md:duration-300 font-semibold
+                ${BTN_COLORS[color]}
+                `}
+                href={href}
+              >
+                {label}
+              </a>
+            ))}
+          </div>
+          <div class="grid flex-row wrap justify-center gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            {cards?.map((
+              { icon, title, description }: CardProps,
+              index: number,
+            ) => (
+              <div
+                key={index}
+                class="bg-[#000D0D] flex flex-col justify-center items-start gap-2 md:gap-6 p-6 md:p-10 py-12 text-[#FFFFFF] rounded-2xl"
+              >
+                {icon && <div className="rounded-full p-1 bg-[#113032]"><img src={icon} width={32} height={32} /></div>}
+                {title && (
+                  <p class="font-semibold mt-2 md:mt-0 text-[24px]">{title}</p>
+                )}
+                {description && <p class="text-[20px] text-[#A1A1AA]">{description}</p>}
+              </div>
+            ))}
+          </div>
+          { bottomMessage ? (
+            <div className="text-[30px] text-center">
+              <HTMLRenderer html={bottomMessage} />
+            </div>
+          ) : null }
+        </div>
+      ))}
     </div>
   );
 }
