c58f1fc94661e7b0702e36fd5a779234a859a103
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2024-05-28 19:24:44 -0300

    Fix: Ajusta a mensagem

--- islands/Clipboard.tsx
+++ islands/Clipboard.tsx
@@ -3,7 +3,8 @@ import { scriptAsDataURI } from "apps/utils/dataURI.ts";
 export default function Clipboard() {
   function setup () {
     return document?.querySelector('#clipboard-btn')?.addEventListener('click', (item: any) => {
-      navigator.clipboard.writeText("TEXTO COPIADO");
+      navigator.clipboard.writeText(`Hello!
+      I'd like to invite you to deco.cx's Hackathon: HTMX edition. For more information, visit: https://deco.cx/hackathon5`);
       item.target.innerHTML = "<img src='https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10325/c1739bf9-c00c-4a01-9f11-3d18e5b683c1' alt='check' width='20'>Copied invite!";
       setTimeout(function() {
         item.target.innerHTML = 'Copy invite';
