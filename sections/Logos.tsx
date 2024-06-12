7dc2e8b4f1838181649b4d48a924a1b4508cec14
Martin Adam <martin.viana99@gmail.com>
2024-06-12 17:29:45 -0300

    Feat: Adding Sponsors Carousel

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -8,6 +8,7 @@ export interface Logo {
 }
 
 export interface Props {
+  /** @format rich-text */
   title?: string;
   logos?: Logo[];
 }
@@ -19,30 +20,42 @@ const IMG_PLACEHODLER = Array(30).fill(0).map(() => ({
 }));
 
 export default function Logos({
-  title = "Edit this heading however you want",
+  title = "Event <span class='argentPixel text-[#02F67C] font-normal'>sponsors</span>",
   logos = IMG_PLACEHODLER,
 }: Props) {
   const slideContent = (
-    <div class="flex items-center gap-20">
+    <div class="flex items-center gap-4">
       {logos?.map((logo) => {
         return (
           <Image
+            class="p-[15px] object-scale-down"
             src={logo.src || ""}
             alt={logo.altText || ""}
-            width={110}
-            height={25}
+            width={144}
+            height={60}
           />
         );
       })}
     </div>
   );
   return (
-    <div class="container lg:mx-auto mx-4 py-6 lg:py-14">
-      <div class="flex flex-col gap-12">
-        <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
-            {slideContent}
+    <div class="bg-black w-full px-6 md:px-8 lg:px-16 py-10 md:py-20">
+      <div class="container lg:mx-auto">
+        <div class="flex flex-col gap-[60px]">
+          <p
+            class="text-center text-10 md:text-[3rem] leading-snug tracking-[-1.2px] md:trancking-[-1.44px] font-medium text-white"
+            dangerouslySetInnerHTML={{
+              __html: title,
+            }}
+          />
+          <div class="relative w-full overflow-hidden h-11">
+            <div className="before:absolute before:inset-y-0 before:left-0 before:w-[70px] lg:before:w-46 before:bg-gradient-to-l before:from-black/0 before:to-[#000000] before:z-20">
+            </div>
+            <div className="after:absolute after:inset-y-0 after:right-0 after:w-[70px] lg:after:w-46 after:bg-gradient-to-r after:from-black/0 after:to-[#000000] after:z-20">
+            </div>
+            <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+              {slideContent}
+            </div>
           </div>
         </div>
       </div>
