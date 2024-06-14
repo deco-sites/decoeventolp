196ca42122e412f51279ba3bb909eeedd69aacaa
Martin Adam <martin.viana99@gmail.com>
2024-06-14 18:14:42 -0300

    force deploy

--- islands/SponsorMessage.tsx
+++ islands/SponsorMessage.tsx
@@ -4,7 +4,7 @@ export default function Clipboard() {
   function setup () {
     return document?.querySelector('#sponsorMessage')?.addEventListener('click', (item: any) => {
       const msgCopied = item.target.cloneNode(true);
-      msgCopied.className = "absolute mt-80 bg-transparent text-[#fff] border-transparent flex items-center justify-center text-center w-min whitespace-nowrap h-[50px] gap-2 font-medium text-lg"
+      msgCopied.className = "absolute mt-80 bg-transparent text-white border-transparent flex items-center justify-center text-center w-min whitespace-nowrap h-[50px] gap-2 font-medium text-lg"
       msgCopied.innerHTML = "Please send an email to <span class='font-bold'>cecilia@deco.cx</span>";
       item.target.parentNode?.appendChild(msgCopied)
       setTimeout(function() {
